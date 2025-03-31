4bdbfaded6a57d957da528b7162ddd6c2d0aa73d
LETRYKA <helloaryka@gmail.com>
2025-03-31 09:06:53 +0800

    corssssss

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,15 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
+
 
 await connectToDatabase();
 mongoose.connect(process.env.MONGODB_STRING);
